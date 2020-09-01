3ab9af2d7790558c6426ac225127015572b13b32
Daniel Chavez <dgchavez2@miners.utep.edu>
2020-08-31 20:37:36 -0600

    Not working, but my solution is here. working out kinks

---
+++ wordCount.py
@@ -0,0 +1,52 @@
+#! /usr/bin/ python3
+
+import sys
+import re
+import os
+import subprocess
+
+inputFname = sys.argv[1]
+outputFname = sys.argv[2]
+
+def main():
+    #reading file
+    file = open(inputFname, "r")
+    document = file.read()
+    file.close()
+
+    #ignoring case
+    document = document.lower()
+
+    #calls func and sorts the returned list, initializing dictionary
+    # to get ready for output file
+    word_list = get_words(document)
+    word_list = sorted(word_list)
+    dic = {}
+
+    #counts words
+    for word in document:
+        if word not in dic:
+                dic[word] = 1
+        else:
+            dic[word] += 1
+
+    file = open(outputFname, "w")
+
+    for word in dic:
+        file.write(word + " " + str(dic.get(word)) + "\n")
+
+    file.close()
+
+#func that returns list of words with no special chars
+def get_words(document):
+    word_list = []
+
+    #regex substitutes all non words or underscores with a space
+    #since \W doesn't include _ i had to include or statement
+    re.sub(r"\W+|_", " ", document)
+
+    for word in document:
+        word_list.append(word)
+    return word_list
+
+main()
